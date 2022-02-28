795f5a808d684427d99efd8b1ff229dbffabbbd3
Hossen <mdmitho199911@gmail.com>
2022-03-01 00:45:45 +0600

    search  but js done

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const InputValue = () => {
     const searchInput = document.getElementById('search-input').value
     searchBtn(searchInput)
-    console.log()
 }
 const searchBtn = (searchText) =>{
     // const searchBtn = document.getElementById('search-btn')
@@ -9,10 +8,41 @@ const searchBtn = (searchText) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.data[0]))
+    .then(phone => displayPhone(phone.data))
  
 }
-const displayPhone = phone =>{
+const displayPhone = phones => {
     const cardContainer = document.getElementById('card-container')
-    
+    cardContainer.textContent=''
+ phones?.forEach(phone => {
+    console.log(phone)
+    const div = document.createElement('div')
+   
+    div.innerHTML = `
+    <div class="col">
+    <div class="card">
+      <img src="${phone.image}" class="card-img-top w-50 mx-auto">
+      <div class="card-body">
+        <h5 class="card-title">Phone Name: ${phone.phone_name}</h5>
+        <p class="card-text">Brand: ${phone.brand}</p>
+        <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-primary mx-auto">DETAIL</button>
+      </div>
+    </div>
+  </div>
+  `
+  cardContainer.appendChild(div)
+ });
+ 
+}
+
+const loadPhoneDetail = idName =>{
+    console.log(idName)
+  const url =`https://openapi.programming-hero.com/api/phone/${idName}`
+  fetch(url)
+  .then (res => res.json())
+  .then(data => displayPhoneDetail(data))
+
+}
+const displayPhoneDetail = detail => {
+    console.log(detail)
 }
