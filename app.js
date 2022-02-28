c1ba058f03b651637d7dbb2520a00ecea1a0c7a4
Hossen <mdmitho199911@gmail.com>
2022-03-01 02:33:28 +0600

    detail btn done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const InputValue = () => {
     const searchInput = document.getElementById('search-input').value
     searchBtn(searchInput)
+    document.getElementById('search-input').value=''
 }
 const searchBtn = (searchText) =>{
     // const searchBtn = document.getElementById('search-btn')
@@ -12,10 +13,11 @@ const searchBtn = (searchText) =>{
  
 }
 const displayPhone = phones => {
+    console.log(phones)
     const cardContainer = document.getElementById('card-container')
     cardContainer.textContent=''
  phones?.forEach(phone => {
-    console.log(phone)
+ 
     const div = document.createElement('div')
    
     div.innerHTML = `
@@ -36,13 +38,48 @@ const displayPhone = phones => {
 }
 
 const loadPhoneDetail = idName =>{
-    console.log(idName)
   const url =`https://openapi.programming-hero.com/api/phone/${idName}`
   fetch(url)
   .then (res => res.json())
-  .then(data => displayPhoneDetail(data))
+  .then(detail => displayPhoneDetail(detail.data))
 
 }
-const displayPhoneDetail = detail => {
-    console.log(detail)
+
+const displayPhoneDetail = detalils=> {
+    const detailContainer = document.getElementById('detail-container')
+    detailContainer.textContent = ''
+    console.log(detalils)
+     const sensors = (detalils.mainFeatures.sensors)
+    const others = (detalils.others)
+console.log(others)
+const div= document.createElement('div') 
+    div.innerHTML = `
+
+
+    <div class="col">
+    <div class="card w-50 mx-auto">
+    <img src="${detalils.image}" class="card-img-top w-50 mx-auto">
+      <div class="card-body">
+      <h3 class="text-center">SENSORS</h3>
+      <p class="text-center">${sensors[0]}<br>${sensors[1]}<br>${sensors[2]}<br>${sensors[3]}<br>${sensors[4]}<br>${sensors[5]}</p>
+      <h3 class="text-center">OTHERS</h3>
+      <p class="text-center">Bluetooth : ${others.Bluetooth}<br>
+       GPS : ${others.GPS}<br>
+       NFC: ${others.NFC}<br>
+       Radio : ${others.Radio}<br>
+       USB  : ${others.USB}<br>
+       WLAN : ${others.WLAN}
+        </p>
+      </div>
+    </div>
+  </div>
+
+
+
+     
+    
+  `
+  detailContainer.appendChild(div)
+    
 }
+ 
